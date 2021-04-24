c7e7075ac7bd2dfbd64c6fbd162b8aada340fb1c
justinekyle <justinekyle@gmail.com>
2021-04-24 18:23:04 +0800

    First Upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+    <center><h1> PHP</h1></center>
+
+    <?php
+        // comment
+        echo "<center><h2>PHP</h2></center>";
+
+        /*if ("10" !== 10){
+            echo "<center>Not the same Data type</center>";
+        }*/
+
+    ?>
+
+    <center><h3>PHP</h3></center>
+
+    </body>
+
+</html>
